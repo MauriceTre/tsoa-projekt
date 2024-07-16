47f9e6a3ab5044fe5588154ecab6bf5aae9ce2de
MauriceTre <140593975+MauriceTre@users.noreply.github.com>
2024-07-16 12:24:15 +0200

    change swagger.ts

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,26 +1,26 @@
-import swaggerJsDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import { Express } from 'express';
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
 
 const swaggerOptions = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Express API with Swagger',
-      version: '1.0.0',
-      description: 'A simple Express API application documented with Swagger',
+      title: "Express API with Swagger",
+      version: "1.0.0",
+      description: "A simple Express API application documented with Swagger",
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: "http://localhost:3000",
       },
     ],
   },
-  apis: ['./src/routes/*.ts'], // Pfad zu den API-Dokumentationsdateien
+  apis: ["./src/routes/*.ts"], // Pfad zu den API-Dokumentationsdateien
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 export const setupSwagger = (app: Express) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 };
